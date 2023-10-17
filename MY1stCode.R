4f5e1c87e7bdc8de0a5061fe65a342fbfe16bbed
Ubanwani <148264601+Ubanwani@users.noreply.github.com>
2023-10-17 15:36:13 -0600

    Add files via upload

---
+++ MY1stCode.R
@@ -0,0 +1,16 @@
+# creating variable
+  y <- 10^2
+a <- "predictive analysis"
+z <- 10.26
+print(z)
+
+# vector
+myvector <- c(1,2,3,4,5)
+print(myvector)
+print(myvector[4])
+
+foo <- 20
+bar <- 50
+myNewvec <- c(foo, bar)
+myOtherVec <- seq(from=3, to=27, by=3)
+
